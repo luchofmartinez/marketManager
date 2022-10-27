d8218fc5e30996264c1671481431f1d66507c1d4
Luciano Martinez <lucianomartinez@outlook.com>
2022-10-26 23:41:38 -0300

    Add icons to the menu and assets to the project

--- lib/widgets/Menu.dart
+++ lib/widgets/Menu.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:market_manager/utils/Assets.dart';
 
 class Menu extends StatelessWidget {
   const Menu({super.key, required this.title});
@@ -22,12 +23,15 @@ class Menu extends StatelessWidget {
           children: [
             const DrawerHeader(
               decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-              child: Text('Drawer Header'),
+                  color: Colors.blue,
+                  image: DecorationImage(
+                      image: AssetImage(Assets.MARKET_MENU_BACKGROUND),
+                      fit: BoxFit.fill)),
+              child: Text(''),
             ),
             ListTile(
-              title: const Text('Item 1'),
+              leading: Icon(Icons.account_box),
+              title: const Text('Perfil'),
               onTap: () {
                 // Update the state of the app
                 // ...
@@ -36,7 +40,38 @@ class Menu extends StatelessWidget {
               },
             ),
             ListTile(
-              title: const Text('Item 2'),
+              leading: Icon(Icons.account_balance_wallet_outlined),
+              title: const Text('Contabilidad'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.add_shopping_cart),
+              title: const Text('Compras'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.notes),
+              title: const Text('Notas'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: const Text('Configuracion'),
               onTap: () {
                 // Update the state of the app
                 // ...
