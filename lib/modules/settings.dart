b9c0c9a3d3a7b3845eb9b8c29a67c0026809518b
Luciano Martinez <lucianomartinez@outlook.com>
2022-10-30 14:27:17 -0300

    Add settings module

---
+++ lib/modules/settings.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SettingsModule extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: AppBar(
+        title: Text('Settings'),
+      ),
+    );
+  }
+}
